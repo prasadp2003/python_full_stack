6662e0ac040018dd2338cef58d26a83c41167dd0
unknown <prasadprasad11197@gmail.com>
2025-09-08 10:58:44 +0530

    my first commit

---
+++ bub.py
@@ -0,0 +1,8 @@
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap if the element found is greater than the next element
+    
